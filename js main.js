aa26b68cd85fc7c0c08afee66fb2072ee6e7efce
camilavitoriafs <127507578+camilavitoriafs@users.noreply.github.com>
2024-03-15 11:13:29 -0300

    Update js main.js

--- js main.js
+++ js main.js
@@ -1,2 +1,11 @@
 const botoes= querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i= 0; i< botoes.length; i++) {
+    botoes[i].onclick = function(){
+        for (j=; j< botoes.length; j++){
+            botoes[j].remove ("ativo");
+        }
+    botoes[i].classlist.add("ativo");
+    }
+  
+}
